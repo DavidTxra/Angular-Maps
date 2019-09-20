14ee225986c1e8102fc15539d3d4e7590744cc6d
DavidTxra <david.teixeira@edu.devinci.fr>
2019-09-20 16:43:23 +0200

    Commit - Créer un composant angulaire et un routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DonorListComponent } from './donor-list/donor-list.component';
+import { AddDonorComponent } from './add-donor/add-donor.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'donor-list', component: DonorListComponent },
+  { path: 'add-donor/:lat/:lng', component: AddDonorComponent },
+  { path: '',
+    redirectTo: '/donor-list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonorListComponent,
+    AddDonorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
